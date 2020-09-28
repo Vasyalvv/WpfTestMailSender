c53fa1b12e1b8525f0fd96abc6a769e7fc6fac7e
Vasiliy Likhobabin <vasyalvv@gmail.com>
2020-09-28 21:12:45 +0600

    Подключены тестовые данные к интерфейсу

--- WpfMailSender/Models/Server.cs
+++ WpfMailSender/Models/Server.cs
@@ -10,7 +10,7 @@ namespace WpfMailSender.Models
     {
         public string Address { get; set; }
 
-        private int _Port;
+        private int _Port=25;
 
         public int Port
         {
