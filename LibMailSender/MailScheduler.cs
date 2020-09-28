67610455656f5887e83a4c83b3176e843a44a390
Vasiliy Likhobabin <vasyalvv@gmail.com>
2020-09-28 23:45:32 +0600

    Мелкие изменения

--- LibMailSender/MailScheduler.cs
+++ LibMailSender/MailScheduler.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Threading;
 
 namespace LibMailSender
 {
-    class MailScheduler
+    public class MailScheduler
     {
+
     }
 }
