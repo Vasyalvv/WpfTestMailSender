4df34df3b7bbff8f5294c275ccb5c170a87f891c
Vasiliy Likhobabin <vasyalvv@gmail.com>
2020-10-01 20:15:47 +0600

    Реализация модели-представления

---
+++ WpfTestMailSender/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfTestMailSender.ViewModels.Base;
+
+namespace WpfTestMailSender.ViewModels
+{
+    class MainWindowViewModel:ViewModel
+    {
+        private string _Title = "Тестовое окно";
+
+        public string Title {
+            get => _Title;
+            set
+            {
+                if (_Title == value) return;
+                _Title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+    }
+}
