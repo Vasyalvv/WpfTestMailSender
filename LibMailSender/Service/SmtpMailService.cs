a8851bd590eb01e3bc56ba21b5633375f59236ab
Vasiliy Likhobabin <vasyalvv@gmail.com>
2020-10-01 23:47:22 +0600

    Команды управления серверами и сервис отправки почты

---
+++ LibMailSender/Service/SmtpMailService.cs
@@ -0,0 +1,72 @@
+﻿using LibMailSender.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibMailSender.Service
+{
+    public class SmtpMailService : IMailService
+    {
+        public IMailSender GetSender(string Server, int Port, bool SSl, string Login, string Password)
+        {
+            return new SmtpMailSender(Server, Port, SSl, Login, Password);
+        }
+    }
+
+    internal class SmtpMailSender: IMailSender
+    {
+        private readonly string _Address;
+        private readonly int _Port;
+        private readonly bool _SSL;
+        private readonly string _Login;
+        private readonly string _Password;
+
+        public SmtpMailSender(string Address, int Port, bool SSl, string Login, string Password)
+        {
+            _Address = Address;
+            _Port = Port;
+            _SSL = SSl;
+            _Login = Login;
+            _Password = Password;
+        }
+
+        public void Send(string SenderAddress, string RecipientAddress, string Subject, string Body)
+        {
+            MailAddress from = new MailAddress(SenderAddress);
+            MailAddress to = new MailAddress(RecipientAddress);
+
+            using (MailMessage message = new MailMessage(from, to))
+            {
+
+                message.Subject = Subject;
+                message.Body = Body;
+
+                using (SmtpClient client = new SmtpClient(_Address, _Port))
+                {
+                    client.EnableSsl = _SSL;
+
+                    client.Credentials = new NetworkCredential
+                    {
+                        UserName = _Login,
+                        Password = _Password
+                    };
+
+                    try
+                    {
+                        client.Send(message);
+                    }
+                    catch (SmtpException e)
+                    {
+                        Trace.TraceError(e.ToString());
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
--- WpfMailSender/ViewModels/WpfMailSenderWindowViewModel.cs
+++ WpfMailSender/ViewModels/WpfMailSenderWindowViewModel.cs
@@ -4,14 +4,20 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using WpfMailSender.Data;
+using WpfMailSender.Infrastructure.Commands;
 using WpfMailSender.Models;
 using WpfMailSender.ViewModels.Base;
 
 namespace WpfMailSender.ViewModels
 {
-    class WpfMailSenderWindowViewModel:ViewModel
+    class WpfMailSenderWindowViewModel : ViewModel
     {
+        #region Свойства/Поля
+
+
         private string _Title = "Тестовое окно";
 
         public string Title
@@ -28,7 +34,7 @@ namespace WpfMailSender.ViewModels
         public ObservableCollection<Server> Servers
         {
             get => _Servers;
-            set => Set(ref _Servers,value);
+            set => Set(ref _Servers, value);
         }
         public ObservableCollection<Sender> Senders
         {
@@ -45,19 +51,75 @@ namespace WpfMailSender.ViewModels
             get => _Messages;
             set => Set(ref _Messages, value);
         }
-        
+
 
         private Server _SelectedServer;
-        public Server SelectedServer { get => _SelectedServer; set => Set(ref _SelectedServer, value); }            
-                                                                                                                    
-        private Sender _SelectedSender;                                                                             
-        public Sender SelectedSender { get => _SelectedSender; set => Set(ref _SelectedSender, value); }            
-                                                                                                                    
-        private Message _SelectedMessage;                                                                           
-        public Message SelectedMessage { get => _SelectedMessage; set => Set(ref _SelectedMessage, value); }        
-                                                                                                                    
-        private Recipient _SelectedRecipient;                                                                       
+        public Server SelectedServer { get => _SelectedServer; set => Set(ref _SelectedServer, value); }
+
+        private Sender _SelectedSender;
+        public Sender SelectedSender { get => _SelectedSender; set => Set(ref _SelectedSender, value); }
+
+        private Message _SelectedMessage;
+        public Message SelectedMessage { get => _SelectedMessage; set => Set(ref _SelectedMessage, value); }
+
+        private Recipient _SelectedRecipient;
         public Recipient SelectedRecipient { get => _SelectedRecipient; set => Set(ref _SelectedRecipient, value); }
+
+        #endregion
+
+
+        #region Команды
+
+        #region CreateNewServerCommand
+
+        private ICommand _CreateNewServerCommand;
+        public ICommand CreateNewServerCommand => _CreateNewServerCommand
+            ?? new LambdaCommand(OnCreateNewServerCommandExecute, CanCreateNewServerCommandExecute);
+
+        private bool CanCreateNewServerCommandExecute(object arg) => true;
+
+        private void OnCreateNewServerCommandExecute(object obj)
+        {
+            MessageBox.Show("Создание нового сервера");
+        }
+        #endregion
+
+        #region EditServerCommand
+
+        private ICommand _EditServerCommand;
+        public ICommand EditServerCommand => _EditServerCommand
+            ?? new LambdaCommand(OnEditServerCommandExecute, CanEditServerCommandExecute);
+
+        private bool CanEditServerCommandExecute(object arg) => arg is Server || SelectedServer!=null;
+
+        private void OnEditServerCommandExecute(object obj)
+        {
+            Server server = obj as Server ?? SelectedServer;
+            if (server is null) return;
+            MessageBox.Show("Редактирование сервера");
+        }
+        #endregion
+
+        #region DeleteServerCommand
+
+        private ICommand _DeleteServerCommand;
+        public ICommand DeleteServerCommand => _DeleteServerCommand
+            ?? new LambdaCommand(OnDeleteServerCommandExecute, CanDeleteServerCommandExecute);
+
+        private bool CanDeleteServerCommandExecute(object arg) => arg is Server || SelectedServer != null;
+
+        private void OnDeleteServerCommandExecute(object obj)
+        {
+            Server server = obj as Server ?? SelectedServer;
+            if (server is null) return;
+
+            Servers.Remove(server);
+            SelectedServer = Servers.FirstOrDefault();
+        }
+        #endregion
+
+
+        #endregion
 
         public WpfMailSenderWindowViewModel()
         {
