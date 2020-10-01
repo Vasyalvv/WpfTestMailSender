92b348d389220b1a3bbeda28a4a3e181035358f3
Vasiliy Likhobabin <vasyalvv@gmail.com>
2020-10-01 21:50:35 +0600

    Перенос инфраструктуры

---
+++ WpfMailSender/ViewModels/WpfMailSenderWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMailSender.ViewModels.Base;
+
+namespace WpfMailSender.ViewModels
+{
+    class WpfMailSenderWindowViewModel:ViewModel
+    {
+        private string _Title = "Тестовое окно";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+    }
+}
