46c00cf82143a35d55ea068e436b3c96605f19b6
Vasiliy Likhobabin <vasyalvv@gmail.com>
2020-10-01 21:10:42 +0600

    Базовый класс команды

---
+++ WpfTestMailSender/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfTestMailSender.Infrastructure.Commands.Base
+{
+    abstract class Command : ICommand
+    {
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        bool ICommand.CanExecute(object parameter) => CanExecute(parameter);
+
+        void ICommand.Execute(object parameter) => Execute(parameter);
+
+        protected virtual bool CanExecute(object p) => true;
+
+        protected abstract void Execute(object p);
+    }
+}
